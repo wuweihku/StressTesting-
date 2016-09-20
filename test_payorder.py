df874ba27fe34f9c3ddf150d686e02b4df5f6ae8
root <root@localhost>
2016-09-20 16:31:48 +0800

    优化打印输出

--- test_payorder.py
+++ test_payorder.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-import unittest                                         #支持Python单元测试模块
+import unittest                                         #支持python单元测试模块
 import urllib.parse                                     #这里urllib.parse要精确到子模块,否则会报错
 import urllib.request                                   #这里urllib.request要精确到子模块,否则会报错
-import csv                                              #支持csv自动化导表
-import sys                                              #支持log
+import csv                                              #支持csv
 import time                                             #支持时间戳
 import hashlib                                          #支持MD5加密
 
@@ -26,7 +25,7 @@ class test_payorder(unittest.TestCase):
     def test_payorder_cases(self):                      #执行测试功能的函数
         with open('csv/payorder_data.csv') as csvfile:  #打开csv文件流
             reader = csv.DictReader(csvfile,skipinitialspace=True)            #创建文件流对象,skipinitialspace忽略逗号后的空格，支持extension
-            totalnum = 0                                  #计算一共跑了多少条测试数据
+            totalnum = 0                                #计算一共跑了多少条测试数据
             for row in reader:                          #这里的row对应csv表里的一行数据,第一行数据自动作为字段名,第二行数据开始作为测试实例
                 totalnum += 1
                 with self.subTest(row=row):             #row=i,会报错row is not defined,必须用row=row(这里用的是subTest功能)
@@ -60,22 +59,14 @@ class test_payorder(unittest.TestCase):
                             'signType':row['signType'],
                             'signature':row['signature']
                             } #csv里的每一行测试实例，这里不用过滤空值，空值可以作为测试用例，引发异常. 注意：为了保证压测性能，这里的每一次读取，应该都是完备的数据，不需要额外处理.
-                    info['extension'] = {'acessToken': 'Xy8HVpI8LnJfxywHjy_oWIKzdvx4WvbQ', 'uid': 1303829}
-                    print(info)
+                    
                     postdata = urllib.parse.urlencode(info).encode('utf-8')         #将信息编码成urllib能够识别的类型,注意的是python2.7用的ASCII编码,python3.X要UTF8转码 
                     response = urllib.request.urlopen(test_payorder.url,postdata).read()          #服务器响应的字符串消息
                     response_dict = eval(response);                                 #转换成字典后的消息
-                    print(response_dict);
                     self.assertEqual(response_dict['code'], eval(row['code']))      #IOS内购获取订单号接口模块---您看到此信息,代表当行测试数据未通过---  
-        print("----------------------------------------------------------------------------------------------------------------------")
-        print(response_dict['data'])
-        print("----------------------------------------------------------------------------------------------------------------------")
 '''
 unittest.main(),固定格式,用于默认调用unittest模块
 '''
 if __name__ == '__main__':
-    log_file = 'log/log_%s.txt'%time.strftime("%Y_%m_%d_%H:%M:%S", time.localtime())#定义log路径及文件名
-    f = open(log_file, "w")
-    runner = unittest.TextTestRunner(f)
-    unittest.main(testRunner=runner)
-    f.close()
+    unittest.main()
+
