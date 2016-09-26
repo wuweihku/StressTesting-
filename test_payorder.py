f2a1430391f3eb2b3935db62d5eb7f0ca413a912
root <root@localhost>
2016-09-26 14:24:04 +0800

    加了多线程和队列的初始框架

--- test_payorder.py
+++ test_payorder.py
@@ -5,7 +5,8 @@ import urllib.request                                   #这里urllib.request要
 import csv                                              #支持csv
 import time                                             #支持时间戳
 import hashlib                                          #支持MD5加密
-
+import queue                                            #支持队列，python3中用小写queue
+import threading                                        #支持多线程
 
 '''
 IOS内购获取订单号接口
@@ -29,7 +30,7 @@ class test_payorder(unittest.TestCase):
             for row in reader:                          #这里的row对应csv表里的一行数据,第一行数据自动作为字段名,第二行数据开始作为测试实例
                 totalnum += 1
                 with self.subTest(row=row):             #row=i,会报错row is not defined,必须用row=row(这里用的是subTest功能)
-                    print("正在执行第 %d 条测试数据"% totalnum)     #每跑一条数据,显示一次当前进度
+                    print('正在执行第 %d 条测试数据'% totalnum)     #每跑一条数据,显示一次当前进度
                     info = {'action': row['action'], 
                             'userID': row['userID'], 
                             'username':row['username'], 
@@ -64,9 +65,85 @@ class test_payorder(unittest.TestCase):
                     response = urllib.request.urlopen(test_payorder.url,postdata).read()          #服务器响应的字符串消息
                     response_dict = eval(response);                                 #转换成字典后的消息
                     self.assertEqual(response_dict['code'], eval(row['code']))      #IOS内购获取订单号接口模块---您看到此信息,代表当行测试数据未通过---  
+
+'''
+Python的Queue模块中提供了同步的、线程安全的队列类
+
+'''
+class myThread(threading.Thread):           #使用Threading模块创建线程，直接从threading.Thread继承，然后重写__init__方法和run方法：
+    def __init__(self, threadID, name, q):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = 'Thread-%s'%name
+        self.q = q
+    def run(self):
+        print('Starting ' + self.name)
+        process_data(self.name, self.q)
+        print('Exiting ' + self.name)
+
 '''
 unittest.main(),固定格式,用于默认调用unittest模块
 '''
 if __name__ == '__main__':
-    unittest.main()
+
+    exitFlag = 0 
+
+# 定义线程内部run()函数
+    def process_data(threadName, q):
+        while not exitFlag:
+            queueLock.acquire()             #获得锁，成功获得锁定后返回True,可选的timeout参数不填时将一直阻塞直到获得锁定,否则超时后将返回False
+            if not workQueue.empty():
+                data = q.get()
+                queueLock.release()         #释放锁
+                print('%s processing %s' % (threadName, data))
+            else:
+                queueLock.release()
+            time.sleep(1)
+
+    threadList = list(range(1, 3))         #起多少个线程
+    nameList = ["One", "Two", "Three", "Four", "Five"]
+    queueLock = threading.Lock()            #创建线程锁
+    workQueue = queue.Queue(30)             #创建队列
+    threads = []                            #线程列表
+    threadID = 1
+
+# 创建新线程
+    for tName in threadList:
+        thread = myThread(threadID, tName, workQueue)   #起线程
+        thread.start()
+        threads.append(thread)              #将线程都加到线程列表
+        threadID += 1
+
+# 填充队列
+    queueLock.acquire()
+    while not workQueue.full():
+        for word in nameList:
+            workQueue.put(word)                 #将nameList列表里的元素放进队列
+    queueLock.release()
+
+# 等待队列清空
+    while not workQueue.empty():
+        pass
+
+# 通知线程是时候退出
+    exitFlag = 1
+
+# 等待所有线程完成
+    for t in threads:
+        t.join()    # 等待至线程中止.这阻塞调用线程直至线程的join() 方法被调用中止-正常退出或者抛出未处理的异常-或者是可选的超时发生.
+    print('Exiting Main Thread')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
