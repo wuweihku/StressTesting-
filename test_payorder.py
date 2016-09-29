75e1fd6f276f97d5134b72c84705af43fa6912d4
root <root@localhost>
2016-09-29 15:41:09 +0800

    完成调整

--- test_payorder.py
+++ test_payorder.py
@@ -11,6 +11,7 @@ import threading                                        #支持多线程
 TestAPI类:
 __init__()构造函数
 assertAPI()断言函数
+test_payorder()测试payorder接口的函数
 
 '''
 class TestAPI():                                        
@@ -22,8 +23,8 @@ class TestAPI():
     def assertAPI(self, res, esp):                      
         response = res                                  #返回的json转字典
         espect = esp                                    #队列里的一行case
-        global Successed                    
-        global Failed
+        global Successed                                #标记成功次数
+        global Failed                                   #标记失败次数
 
         if int(response['code']) == int(espect['code']):                          #断言成功
             Successed += 1
@@ -32,8 +33,8 @@ class TestAPI():
             Failed += 1
             print('-----------------\n\n','            Failed:  responsecode-> %s and espectcode-> %s, msg: %s '%(response['code'], espect['code'], response['msg']))
             
-            print('%s Processing: '%self.threadName,        #输出当前哪个线程在处理队列中的哪条数据，设计成队列数据按顺序输出处理
-                  'action->%s:'%self.data['action'],
+            print('%s Processing: '%self.threadName,    #输出当前哪个线程在处理队列中的哪条数据，设计成队列数据按顺序输出处理，由于涉及服务器响应，所以返回到sys.print出的可能不是按顺序，但是取出post的时候是按顺序的.
+                  'action->%s'%self.data['action'],
                   'userID-> %s' %self.data['userID'], 
                   'username-> %s'%self.data['username'],
                   'roleID-> %s'%self.data['roleID'], 
@@ -73,30 +74,30 @@ class TestAPI():
                }                                        #csv里的每一行测试实例，这里不用过滤空值，空值可以作为测试用例，引发异常. 注意：为了保证压测性能，这里的每一次读取，应该都是完备的数据，不需要额外处理.
                     
       
-        Totalcases += 1                                                                            #Totalcases计数+1
-        postdata = urllib.parse.urlencode(info).encode('utf-8')                                    #将信息编码成urllib能够识别的类型,注意的是python2.7用的ASCII编码,python3.X要UTF8转码 
-        response = urllib.request.urlopen(self.url, postdata).read()                               #服务器响应的字符串消息
-        response_dict = eval(response);                                                            #json转换成字典
+        Totalcases += 1                                                             #Totalcases计数+1
+        postdata = urllib.parse.urlencode(info).encode('utf-8')                     #将信息编码成urllib能够识别的类型,注意的是python2.7用的ASCII编码,python3.X要UTF8转码 
+        response = urllib.request.urlopen(self.url, postdata).read()                #服务器响应的字符串消息
+        response_dict = eval(response);                                             #json转换成字典
         
-        self.assertAPI(response_dict, self.data)                                      #IOS内购获取订单号接口模块---您看到此信息,代表当行测试数据未通过---  
+        self.assertAPI(response_dict, self.data)        #断言
 
 '''
 Python的Queue模块中提供了同步的、线程安全的队列类
 
 '''
-class myThread(threading.Thread):           #使用Threading模块创建线程，直接从threading.Thread继承，然后重写__init__方法和run方法：
+class myThread(threading.Thread):                       #使用Threading模块创建线程，直接从threading.Thread继承，然后重写__init__方法和run方法：
     def __init__(self, threadID, name, q):
         threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = 'Thread-%s'%name
-        self.q = q
+        self.threadID = threadID                        #线程ID
+        self.name = 'Thread-%s'%name                    #线程名
+        self.q = q                                      #队列对象传入
     def run(self):
         print('Starting ' + self.name)
-        process_data(self.name, self.q)
+        process_data(self.name, self.q)                 #线程业务函数
         print('Exiting ' + self.name)
 
 
-if __name__ == '__main__':                  #如果从shell用python执行脚本
+if __name__ == '__main__':                              #如果从shell用python执行脚本
 
     '''
     IOS内购获取订单号接口
@@ -105,44 +106,46 @@ if __name__ == '__main__':                  #如果从shell用python执行脚本
     测试地址：http://payapi.qa.15166.com/pay/order
 
     '''
-    url = 'http://payapi.qa.15166.com/pay/order'            #所要访问的url,一个测试类对应一个url
+    url = 'http://payapi.qa.15166.com/pay/order'        #所要访问的url,一个测试类对应一个url
 
-    global Totalcases                                           #线程同步的问题需要解决
-    global Successed 
-    global Failed 
+    global Totalcases                                   #记录一共跑了多少cases
+    global Successed                                    #成功cases数
+    global Failed                                       #失败cases数
     Totalcases = 0
     Successed = 0
     Failed = 0
-    timestart  = time.time()                                        #开始时间戳
+    timestart  = time.time()                            #开始时间戳
 
     exitFlag = 0 
 
 # 定义线程内部run()函数
     def process_data(threadName, q):
         while not exitFlag:
-            queueLock.acquire()             #获得锁，成功获得锁定后返回True,可选的timeout参数不填时将一直阻塞直到获得锁定,否则超时后将返回False
+            queueLock.acquire()                         #获得锁，成功获得锁定后返回True,可选的timeout参数不填时将一直阻塞直到获得锁定,否则超时后将返回False
             if not workQueue.empty():
-                qdata = q.get()             #qdata为一行队列数据
-                queueLock.release()         #释放锁
+                qdata = q.get()                         #qdata为一行队列数据
+                queueLock.release()                     #释放锁
                 
-                threadtestapi = TestAPI(threadName, url, qdata)         #构造TestAPI对象
-                threadtestapi.test_payorder()               #执行线程对象的测试函数
+                # print('current case gotten: %s '%qdata['action'])                 #输出取得的cases，用以验证队列顺序输出
+
+                threadtestapi = TestAPI(threadName, url, qdata)                     #构造TestAPI对象, qdata为每次传入的一行case
+                threadtestapi.test_payorder()                                       #执行线程对象的测试函数
 
             else:
                 queueLock.release()
-            time.sleep(0.1)
+            time.sleep(0.05)
 
-    threadList = list(range(1, 11))          #起多少个线程
-    queueLock = threading.Lock()            #创建线程锁
-    workQueue = queue.Queue(300)             #创建队列,并设置队列长度，即所需要跑的case数
-    threads = []                            #线程列表
+    threadList = list(range(1, 31))                     #起多少个线程
+    queueLock = threading.Lock()                        #创建线程锁
+    workQueue = queue.Queue(100)                       #创建队列,并设置队列长度，即所需要跑的case数
+    threads = []                                        #线程列表
     threadID = 1
 
 # 创建新线程
     for tName in threadList:
-        thread = myThread(threadID, tName, workQueue)   #起线程
+        thread = myThread(threadID, tName, workQueue)   #起线程,这里线程对象一创建，就会自动调用线程对象里的run（）函数. 所以在这里用time.sleep（）控制线程节奏，几轮，一轮多少并发.
         thread.start()
-        threads.append(thread)              #将线程都加到线程列表
+        threads.append(thread)                          #将线程都加到线程列表
         threadID += 1
 
 # 填充队列,在这里先把csv里的case都读取进队列
@@ -167,14 +170,14 @@ if __name__ == '__main__':                  #如果从shell用python执行脚本
     exitFlag = 1
 
 # 等待所有线程完成
-    for t in threads:
-        t.join()    # 等待至线程中止.这阻塞调用线程直至线程的join() 方法被调用中止-正常退出或者抛出未处理的异常-或者是可选的超时发生.
+    for t in threads:                                   #起线程的时候，把线程都加进了threads列表里
+        t.join()                                        #等待至线程中止.这阻塞调用线程直至线程的join() 方法被调用中止-正常退出或者抛出未处理的异常-或者是可选的超时发生.
     print('Exiting Main Thread')
 
 # 测试报告
     timefinish = time.time()                            #结束时间戳
     timecost = timefinish-timestart
-    print("Totalcases: %d \n"%Totalcases, "Successed: %d \n"%Successed, "   Failed: %d \n"%Failed)
+    print("--------------------\nTotalcases: %d \n"%Totalcases, "Successed: %d \n"%Successed, "   Failed: %d \n"%Failed)
     print('%s seconds cost for testing'%timecost)
 
 
